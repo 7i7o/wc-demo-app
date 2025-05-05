58fe2752be0da8cf129bf91c1b0b3b0224170584
7i7o <mparij@communitylabs.com>
2025-05-05 22:05:11 +0200

    update card widths

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -24,8 +24,8 @@ export default function Card({
 
   const sizes = {
     sm: "py-2 px-4 text-xs w-[200px] space-y-2 rounded-lg",
-    md: "py-4 px-6 text-sm w-[300px] space-y-3 rounded-xl",
-    lg: "py-6 px-8 text-lg w-md space-y-4 rounded-2xl",
+    md: "py-4 px-6 text-sm w-[280px] space-y-3 rounded-xl",
+    lg: "py-6 px-8 text-lg w-[340px] space-y-4 rounded-2xl",
   };
 
   return (
